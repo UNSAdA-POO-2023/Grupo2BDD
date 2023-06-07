df1551789015c9eb0f65068458ddeeab4bf5e4a0
martin-alexis <alexismartin090@gmail.com>
2023-06-07 12:59:50 -0300

    nada

--- src/main/java/com/tpfinal/tpfinal/models/dao/IUsuario.java
+++ src/main/java/com/tpfinal/tpfinal/models/dao/IUsuario.java
@@ -1,5 +1,14 @@
 package com.tpfinal.tpfinal.models.dao;
 
+import com.tpfinal.tpfinal.models.domain.Usuario;
+
+import java.util.List;
+
 public interface IUsuario {
+    List<Usuario> ObtenerUsuariosPorEmail (Usuario usuario);
+    void registrar (Usuario usuario);
+    void editar (Usuario usuario);
+    void eliminar (String email);
+
 
 }
--- src/main/java/com/tpfinal/tpfinal/models/dao/UsuarioImp.java
+++ src/main/java/com/tpfinal/tpfinal/models/dao/UsuarioImp.java
@@ -1,9 +1,11 @@
 package com.tpfinal.tpfinal.models.dao;
 
 import com.tpfinal.tpfinal.models.domain.Usuario;
-import javax.persistence.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Repository
@@ -13,11 +15,34 @@ public class UsuarioImp implements IUsuario {
     @PersistenceContext
     EntityManager entityManager;
 
-
+    @Override
     @Transactional
-    public List<Usuario> getUsuarios() {
-        String query = "SELECT u FROM Usuario u";
-        return entityManager.createQuery(query).getResultList();
+    public List<Usuario> ObtenerUsuariosPorEmail(Usuario usuario) {
+        String query = "SELECT u FROM Usuario u WHERE u.email = :email";
+        List lista = entityManager.createQuery(query).setParameter("email", usuario.getEmail()).getResultList();
+
+        if (lista.isEmpty()) {
+            return null;
+        }
+        return lista;
     }
 
+    @Override
+    @Transactional
+    public void registrar(Usuario usuario) {
+        entityManager.merge(usuario);
+    }
+
+    @Override
+    @Transactional
+    public void editar(Usuario usuario) {
+            entityManager.merge(usuario);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(String email) {
+        Usuario usuario = entityManager.find(Usuario.class, email);
+        entityManager.remove(usuario);
+    }
 }
